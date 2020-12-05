055e11886e16a92f83efe0145551a659c9d83a61
makombekepha <makombekepha@gmail.com>
2020-12-05 19:45:43 +0300

    changing the links to a tags

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React,{useEffect,useState} from 'react'
 import 'materialize-css';
 import './NavBar.css'
 import M from  'materialize-css/dist/js/materialize.min.js';
-import { Link } from 'react-router-dom'
+//import { a } from 'react-router-dom'
 import {NavDropdown} from 'react-bootstrap'
 
 const NavBar = () => {
@@ -40,34 +40,34 @@ const NavBar = () => {
   <li><a href="#!">three</a></li>
 </ul>
       <ul id="nav-mobile" className="hide-on-med-and-down nav-items">
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="aboutus">About</Link></li>
+         <li><a href="/">Home</a></li>
+         <li><a href="aboutus">About</a></li>
          <li>
          <NavDropdown title="Services" id="collasible-nav-dropdown" className="kik">
-        <Link to="#action/3.1">Multilevel BUildings Construction</Link>
-        <Link to="#action/3.2">Repairs and Maintenance</Link>
-        <Link to="#action/3.3">Interior Designs and Installation</Link>
-        <Link to="#action/3.4">Equipment Hire</Link>
-        <Link to="#action/3.2">Excuvator Works</Link>
-        <Link to="#action/3.3">Site Preparation</Link>
-        <Link to="#action/3.4">Dumping</Link>
-        <Link to="#action/3.3">Building Materials Delivery</Link>
-        <Link to="#action/3.4">Road Murraming and Buttimen Standardizing</Link>
+        <a href="#action/3.1">Multilevel BUildings Construction</a>
+        <a href="#action/3.2">Repairs and Maintenance</a>
+        <a href="#action/3.3">Interior Designs and Installation</a>
+        <a href="#action/3.4">Equipment Hire</a>
+        <a href="#action/3.2">Excuvator Works</a>
+        <a href="#action/3.3">Site Preparation</a>
+        <a href="#action/3.4">Dumping</a>
+        <a href="#action/3.3">Building Materials Delivery</a>
+        <a href="#action/3.4">Road Murraming and Buttimen Standardizing</a>
         </NavDropdown>
          </li>
-         <li><Link to="employees">Meet Your Builder</Link></li>
-         <li><Link to="projects">Projects</Link></li>
-         <li><Link to="process">Process</Link></li>
-         <li><Link to="testimonials" >Testimonials</Link></li>
-         <li><Link to="contacts">Contact</Link></li>
+         <li><a href="employees">Meet Your Builder</a></li>
+         <li><a href="projects">Projects</a></li>
+         <li><a href="process">Process</a></li>
+         <li><a href="testimonials" >Testimonials</a></li>
+         <li><a href="contacts">Contact</a></li>
       </ul>
     </div>
  </nav> 
 
   <ul id="slide-out" className={click ? "sidenav":"side-close"}>
     
-  <li><Link onClick={()=>setClick(false)} to="/">Home</Link></li>    
-  <li><Link onClick={()=>setClick(false)} to="aboutus">About</Link></li>
+  <li><a onClick={()=>setClick(false)} href="/">Home</a></li>    
+  <li><a onClick={()=>setClick(false)} href="aboutus">About</a></li>
          
   <li>
     <ul className="collapsible">
@@ -76,25 +76,25 @@ const NavBar = () => {
      <div className="collapsible-header">
        <i className="material-icons" style={{display:'none'}}></i>Services</div>
        <div className="collapsible-body" style={{paddingLeft:'20px',fontSize:'12px'}}>
-        <Link to="#action/3.1">Multilevel BUildings Construction</Link><br/>
-        <Link to="#action/3.2">Repairs and Maintenance</Link><br/>
-        <Link to="#action/3.3">Interior Designs and Installation</Link><br/>
-        <Link to="#action/3.4">Equipment Hire</Link><br/>
-        <Link to="#action/3.2">Excuvator Works</Link><br/>
-        <Link to="#action/3.3">Site Preparation</Link><br/>
-        <Link to="#action/3.4">Dumping</Link><br/>
-        <Link to="#action/3.3">Building Materials Delivery</Link><br/>
-        <Link to="#action/3.4">Road Murraming and Buttimen Standardizing</Link><br/>
+        <a href="#action/3.1">Multilevel BUildings Construction</a><br/>
+        <a href="#action/3.2">Repairs and Maintenance</a><br/>
+        <a href="#action/3.3">Interior Designs and Installation</a><br/>
+        <a href="#action/3.4">Equipment Hire</a><br/>
+        <a href="#action/3.2">Excuvator Works</a><br/>
+        <a href="#action/3.3">Site Preparation</a><br/>
+        <a href="#action/3.4">Dumping</a><br/>
+        <a href="#action/3.3">Building Materials Delivery</a><br/>
+        <a href="#action/3.4">Road Murraming and Buttimen Standardizing</a><br/>
     </div>
       </li>
      
     </ul>
   </li>
-         <li><Link onClick={()=>setClick(false)} to="employees">Meet Your Builder</Link></li>
-         <li><Link onClick={()=>setClick(false)} to="projects">Projects</Link></li>
-         <li><Link onClick={()=>setClick(false)} to="process">Process</Link></li>
-         <li><Link onClick={()=>setClick(false)} to="testimonials">Testimonials</Link></li>
-         <li><Link onClick={()=>setClick(false)} to="contacts">Contact</Link></li>
+         <li><a onClick={()=>setClick(false)} href="employees">Meet Your Builder</a></li>
+         <li><a onClick={()=>setClick(false)} href="projects">Projects</a></li>
+         <li><a onClick={()=>setClick(false)} href="process">Process</a></li>
+         <li><a onClick={()=>setClick(false)} href="testimonials">Testimonials</a></li>
+         <li><a onClick={()=>setClick(false)} href="contacts">Contact</a></li>
   </ul>
   
         </div>
