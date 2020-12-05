ee9b6c87236b7054e1fe9463a05cf61371050a3b
makombekepha <makombekepha@gmail.com>
2020-12-05 22:06:10 +0300

    change navbar totally

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Footer from './components/Footer/Footer';
-import NavBar from './components/NavBar/NavBar';
+//import NavBar from './components/NavBar/NavBar';
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
 import Contacts from './components/Contacts/Contacts';
 import AboutUs from './components/AboutUs/AboutUs';
@@ -10,11 +10,13 @@ import Testimonial from './components/Testimonials/Testimonial';
 import Employees from './components/Employees/Employees';
 import Projects from './components/Projects/Projects';
 import Home from './components/Home/Home';
+import Tnav from './components/Tnav/Tnav';
 
 function App() {
   return (
     <Router className="App">
-      <NavBar />
+      {/*<NavBar />*/}
+      <Tnav />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/contacts' component={Contacts} />
